7e0f36f70fcde71cd8875a5dbf6bbf3529ca41bc
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-06-26 11:55:09 -0600

    setup initial state for card component inputs

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,29 @@ import Input from './input'
 
 
 class Card extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            color: 'Blue',
+            pluralNoun: '',
+        }
+    }
+
+    handleInputChange() {
+        this.setState({ color: 'red' })
+    }
+
+
     render() {
+
+        return(
+            <div className="card">
+                <h1>{this.state.color}</h1>
+                <input/>
+            </div>
+        ) 
         return (
             <div className="card">
                 { Input('color') }
